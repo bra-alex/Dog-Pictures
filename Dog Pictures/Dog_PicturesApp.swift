fa5f6d5fc06b1b6faae90608cd716adf348fa316
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-26 11:26:53 +0000

    Initial Commit

---
+++ Dog Pictures/Dog_PicturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dog_PicturesApp.swift
+//  Dog Pictures
+//
+//  Created by Don Bouncy on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Dog_PicturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
